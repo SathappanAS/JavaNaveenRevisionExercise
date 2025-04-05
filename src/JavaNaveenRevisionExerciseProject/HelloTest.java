22f6c5966ff3123ef436d7f6ebd1fd01646a8ba5
sathappan1989 <sathappanas@outlook.com>
2025-04-04 21:54:05 -0700

    Test

---
+++ src/JavaNaveenRevisionExerciseProject/HelloTest.java
@@ -0,0 +1,12 @@
+package JavaNaveenRevisionExerciseProject;
+
+public class HelloTest {
+
+	
+
+	public static void main(String[] args) {
+		System.out.println("Test");
+
+	}
+
+}
---
+++ src/JavaNaveenRevisionExerciseProject/test.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package JavaNaveenRevisionExerciseProject;
+
+/**
+ * 
+ */
+public class test {
+
+	/**
+	 * 
+	 */
+	public test() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
