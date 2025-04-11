f8e6365266ec0d82209e52d3907240d6bed2b2f4
sathappan1989 <sathappanas@outlook.com>
2025-04-10 23:29:42 -0700

    java

---
+++ src/Java_03_CharType_ASCIIValues_StringDataType_StringConcatenation_ArithmaticOperators/CharConcept.java
@@ -0,0 +1,29 @@
+package Java_03_CharType_ASCIIValues_StringDataType_StringConcatenation_ArithmaticOperators;
+
+public class CharConcept {
+
+	public static void main(String[] args) {
+		
+		int a = 20;
+		int b = 10;
+		
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(b/a);
+
+		char c1 = 'a'; //97
+		char c2 = 'b'; //98
+		System.out.println(c1);
+		System.out.println(c1+c2); //195
+		
+		
+		//ASCII values:
+		
+		
+		
+		
+		
+	}
+
+}
