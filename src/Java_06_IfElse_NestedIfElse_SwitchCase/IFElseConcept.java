c24c50e96e9fe095866dee04f95a7c5dd7b321b3
sathappan1989 <sathappanas@outlook.com>
2025-04-12 21:37:13 -0700

    ifelse

---
+++ src/Java_06_IfElse_NestedIfElse_SwitchCase/IFElseConcept.java
@@ -0,0 +1,22 @@
+package Java_06_IfElse_NestedIfElse_SwitchCase;
+
+public class IFElseConcept {
+
+	public static void main(String[] args) {
+		
+		int a =10;
+		int b=20;
+		
+		// == comparision operator to compare two primitive data types
+		// = assignment operator
+		
+		if(a==b) {
+			System.out.println("pass");
+		}
+		else {
+			System.out.println("fail");
+		}
+
+	}
+
+}
