9c6325fd2e3d8c67315db9173678afcde7e23960
sathappan1989 <sathappanas@outlook.com>
2025-04-12 22:57:10 -0700

    SwitchCase

---
+++ src/Java_06_IfElse_NestedIfElse_SwitchCase/SwitchCaseConcept.java
@@ -0,0 +1,29 @@
+package Java_06_IfElse_NestedIfElse_SwitchCase;
+
+public class SwitchCaseConcept {
+
+	public static void main(String[] args) {
+		
+		String browser = "safari";
+		
+		switch (browser) {
+		case "chrome":
+			System.out.println("launch chrome");
+			break;
+		case "firefox":
+			System.out.println("launch firefox");
+			break;
+		case "edge":
+			System.out.println("launch edge");
+			break;
+		case "safari":
+			System.out.println("launch safari");
+			break;
+		default:
+			System.out.println("invalid browser..pls pass the right browser name..");
+			break;
+		}
+
+	}
+
+}
