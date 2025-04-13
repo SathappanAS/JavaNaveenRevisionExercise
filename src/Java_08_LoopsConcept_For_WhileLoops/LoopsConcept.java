48508c18092db0add73a177f6f6f35cb8bab8fb9
sathappan1989 <sathappanas@outlook.com>
2025-04-13 13:31:14 -0700

    WhileLoops

---
+++ src/Java_08_LoopsConcept_For_WhileLoops/LoopsConcept.java
@@ -0,0 +1,18 @@
+package Java_08_LoopsConcept_For_WhileLoops;
+
+public class LoopsConcept {
+
+	public static void main(String[] args) {
+		
+		// 1. while loop
+		// 1 to 10:
+		
+		int i=1; //initialization
+		while(i<=10) {  //condition checking
+			System.out.println(i);
+			i++;
+		}
+
+	}
+
+}
