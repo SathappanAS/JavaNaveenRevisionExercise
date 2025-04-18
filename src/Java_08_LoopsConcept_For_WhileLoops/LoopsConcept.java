8ad6f71692457bf35e68675ea218ac8af2a3ebd2
sathappan1989 <sathappanas@outlook.com>
2025-04-17 22:02:17 -0700

    Loops

--- src/Java_08_LoopsConcept_For_WhileLoops/LoopsConcept.java
+++ src/Java_08_LoopsConcept_For_WhileLoops/LoopsConcept.java
@@ -20,9 +20,16 @@ public class LoopsConcept {
 //			System.out.println("hi");
 //		}
 		
-		
-		
-		
+		int p=1;
+		while(p<=10) {
+			System.out.println(p);
+			if(p==5) {
+				System.out.println("Hello Selenium");
+				break; //break is allowed only inside the loop. Break will always entire loop
+			}
+			p++;
+		}
+
 	}
 
 }
