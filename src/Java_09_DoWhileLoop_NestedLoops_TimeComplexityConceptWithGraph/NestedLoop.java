eb74a7998b001884cfc40d875342bc83ca49660b
sathappan1989 <sathappanas@outlook.com>
2025-04-18 20:06:12 -0700

    NestedLoops

--- src/Java_09_DoWhileLoop_NestedLoops_TimeComplexityConceptWithGraph/NestedLoop.java
+++ src/Java_09_DoWhileLoop_NestedLoops_TimeComplexityConceptWithGraph/NestedLoop.java
@@ -14,7 +14,7 @@ public class NestedLoop {
 		
 		for(int i=0;i<=5;i++) {
 			for(int j=0;j<6;j++) {
-				System.out.print(String.valueOf(i)+String.valueOf(j)+ " ");
+				System.out.print(i+""+j+ " ");
 			}
 			System.out.println( "  " );
 		}
