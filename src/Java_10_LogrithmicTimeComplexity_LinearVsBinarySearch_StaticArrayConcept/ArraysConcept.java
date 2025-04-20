1e93ecdb4b784c4ad5af0ce950c6d6ed9b550888
sathappan1989 <sathappanas@outlook.com>
2025-04-19 22:36:06 -0700

    Static Arrays

--- src/Java_10_LogrithmicTimeComplexity_LinearVsBinarySearch_StaticArrayConcept/ArraysConcept.java
+++ src/Java_10_LogrithmicTimeComplexity_LinearVsBinarySearch_StaticArrayConcept/ArraysConcept.java
@@ -3,8 +3,25 @@ package Java_10_LogrithmicTimeComplexity_LinearVsBinarySearch_StaticArrayConcept
 public class ArraysConcept {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		//array:
+		//1. new keyword
+		//2. array literals
+		
+		//int array:
+		
+		int i[]=new int[4];
+		
+		i[0]=100;
+		i[1]=200;
+		i[2]=300;
+		i[3]=400;
+		//i[4]=500; // AIOB
+		
+		System.out.println(i[0]); //100
+		System.out.println(i[3]); //300
+		//System.out.println(i[4]); //java.lang.ArrayIndexOutOfBoundsException: AIOB
+		System.out.println(i.length);
 	}
 
 }
