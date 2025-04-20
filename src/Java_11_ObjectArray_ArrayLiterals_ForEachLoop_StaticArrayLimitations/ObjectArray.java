1a3b80e4cbdd309b713b55e404b2d7c0d36d12e1
sathappan1989 <sathappanas@outlook.com>
2025-04-20 14:53:19 -0700

    ObjectArray

---
+++ src/Java_11_ObjectArray_ArrayLiterals_ForEachLoop_StaticArrayLimitations/ObjectArray.java
@@ -0,0 +1,40 @@
+package Java_11_ObjectArray_ArrayLiterals_ForEachLoop_StaticArrayLimitations;
+
+import java.util.Arrays;
+
+public class ObjectArray {
+	public static void main(String[] args) {
+
+		// Object Array : We are store different type of data
+
+		// emp data:
+		// name(String)
+		// age(int)
+		// salary(double)
+		// gender(char)
+		// isPermanent(boolean)
+
+		// Object: is a super(parent) class
+
+		Object empData[] = new Object[5];// 0-4
+		System.out.println(Arrays.toString(empData));
+
+		empData[0] = "Supriya";
+		empData[1] = 30;
+		empData[2] = 34.44;
+		empData[3] = 'f';
+		empData[4] = true;
+		// empData[5] = "IBM";//AIOB
+
+		System.out.println(empData[0]);
+		System.out.println(empData.length);
+
+		System.out.println(Arrays.toString(empData));
+
+		for (Object e : empData) {
+			System.out.println(e);
+		}
+
+	}
+
+}
