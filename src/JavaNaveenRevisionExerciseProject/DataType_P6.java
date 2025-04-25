b1d879added49ccc177fbf8ad1b2668fbba109d6
sathappan1989 <sathappanas@outlook.com>
2025-04-24 22:00:48 -0700

    DataType_P6

--- src/JavaNaveenRevisionExerciseProject/DataType_P6.java
+++ src/JavaNaveenRevisionExerciseProject/DataType_P6.java
@@ -1,9 +1,20 @@
 package JavaNaveenRevisionExerciseProject;
-
+/*Create three int variables having values like : 100, 200, 3400. Add them and concatenate and generate this output String :
+"Your Total amount is: 3700".
+Solution:
+int a = 100;
+int b = 200;
+int c = 3400;
+System.out.println(“Your Total amount is : “ +(a+b+c));*/
 public class DataType_P6 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		int a = 100;
+		int b = 200;
+		int c = 3400;
+		
+		System.out.println("Your Total amount is : "+(a+b+c));
 
 	}
 
