7f071fb8fb15a4946b465d9987bea941105372dc
sathappan1989 <sathappanas@outlook.com>
2025-04-28 19:46:18 -0700

    ConditionalOperators

---
+++ src/ConditionalOperators/QuestionThree.java
@@ -0,0 +1,19 @@
+package ConditionalOperators;
+/*Write a java program to test a number is positive or negative*/
+public class QuestionThree {
+
+	public static void main(String[] args) {
+		
+		int num = -98;
+		
+		if(num>=0) {
+			System.out.println("Number is the positive number : " + num);
+		}
+		else {
+			System.out.println("Number is the negative number : " + num);
+		}
+		
+
+	}
+
+}
--- src/ConditionalOperators/QuestionTwo.java
+++ src/ConditionalOperators/QuestionTwo.java
@@ -3,14 +3,21 @@ package ConditionalOperators;
 public class QuestionTwo {
 
 	public static void main(String[] args) {
-		
+
 		int a = 25;
 		int b = 78;
 		int c = 87;
 		int d = 97;
-		
-		
+
+		if (a > b && a > b && a > c) {
+			System.out.println("The greatest number : " + a);
+		} else if (b > c && b > d) {
+			System.out.println("The greatest number : " + b);
+		} else if (c > d) {
+			System.out.println("The greatest number : " + c);
+		} else {
+			System.out.println("The greatest number : " + d);
+		}
 
 	}
-
 }
